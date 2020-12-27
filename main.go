ba53f77ae7562776e216224881041d5d6faac97f
paolo.carraro <paolo.carraro@ekr.it>
2020-12-27 12:05:50 +0100

    fix error managing with happy path

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 func run() error {
 	pbuilder := &PersonBuilder{}
-	pp, err := pbuilder.SetName("", "Smith").SetAge(165).Build()
+	pp, err := pbuilder.SetName("Paul", "").SetAge(165).Build()
 	if err != nil {
 		return err
 	}
@@ -23,6 +23,7 @@ func run() error {
 	return nil
 }
 
+//Person rappersent a human
 type Person struct {
 	//Name is how person is called
 	Name string
@@ -32,31 +33,36 @@ type Person struct {
 	Age int
 }
 
+//PersonBuilder for buildint a person object safely
 type PersonBuilder struct {
 	p Person
 	e error
 }
 
+//Build is final act of PersonBuilder
 func (pb *PersonBuilder) Build() (Person, error) {
 	return pb.p, pb.e
 }
 
+//SetName check and set person name and surname
 func (pb *PersonBuilder) SetName(name, surname string) *PersonBuilder {
-	if name != "" && surname != "" {
-		pb.p.Name = name
-		pb.p.Surname = surname
-	} else {
+	if name == "" || surname == "" {
 		pb.e = manageError(pb.e, "no valid name")
 	}
+
+	pb.p.Name = name
+	pb.p.Surname = surname
 	return pb
 }
 
+//SetAge check and set person age
 func (pb *PersonBuilder) SetAge(age int) *PersonBuilder {
-	if age > 0 && age < 101 {
-		pb.p.Age = age
-	} else {
+	if age <= 0 || age > 101 {
 		pb.e = manageError(pb.e, "no valid age")
+		return pb
 	}
+
+	pb.p.Age = age
 	return pb
 }
 
